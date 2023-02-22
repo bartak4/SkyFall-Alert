db22b6c9c2bbcc7897f17d7f6013213f50a6ba4d
Bartak, Marek <MBartak@gk-software.com>
2023-02-22 15:11:36 +0100

    Initial Commit

---
+++ SkyFall Alert/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SkyFall Alert
+//
+//  Created by Bartak, Marek on 22.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
